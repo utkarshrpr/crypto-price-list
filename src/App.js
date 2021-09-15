f5a6e35bdfc3a96527dce78da43b9954ec5f716a
Utkarsh Chauhan <utkarshchauhan007@gmail.com>
2021-09-15 13:01:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Coin from './Coin';
+
+
+function App() {
+  const [coins, setCoins] = useState([]); 
+  
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+        // console.log(res.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  });
+
+  return (
+    <div>
+     {coins.map(coin => {
+       return(
+        <Coin
+         key={coin.id}
+         name={coin.name}
+         image={coin.image}
+         symbol={coin.symbol}
+         marketCap={coin.market_cap}
+         price={coin.current_price}
+         priceChange={coin.price_change_percentage_24h}
+         volume={coin.total_volume}
+         />
+       )
+     })}  
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Coin.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Coin = ({ name, image, symbol, price, marketCap, priceChange, volume }) => {
+    var priceChangeTag;
+    if (priceChange < 0)
+        priceChangeTag = <td style={{color: '#f00606', width: '12%' }}>{priceChange.toFixed(2)}%</td>;
+    else
+        priceChangeTag = <td style={{color: '#11d811' }}>{priceChange.toFixed(2)}%</td>;
+
+    return (
+        
+        <tr>
+            <td>
+                <img src={image} style={{width: '20px', height: '20px', marginRight: '10'}} />
+                <span style={{fontSize: '13px'}}><b>{name} </b></span>
+                <span style={{fontSize: '13px', color: '#808a9d'}}>{symbol.toUpperCase()}</span>
+            </td>
+            {priceChangeTag}
+            <td>${price.toFixed(2)}</td>
+            <td>${volume.toLocaleString()}</td>
+            <td>${marketCap.toLocaleString()}</td>
+        </tr>
+        
+    )
+}
+
+export default Coin
